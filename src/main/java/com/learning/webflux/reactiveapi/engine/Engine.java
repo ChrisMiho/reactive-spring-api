107c76969a94c6793786e73d885f0db292199dd8
ChrisMiho <mihovilovich13@gmail.com>
2023-10-22 20:27:22 -0500

    beginning of engine implementation

---
+++ src/main/java/com/learning/webflux/reactiveapi/engine/Engine.java
@@ -0,0 +1,14 @@
+package com.learning.webflux.reactiveapi.engine;
+
+import com.learning.webflux.reactiveapi.model.CatGene;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Engine {
+
+//    public Mono<List<CatGene>> geneProcessor(List<CatGene> catGenes){
+//        return Mono.from()
+//
+//    }
+}
--- src/main/java/com/learning/webflux/reactiveapi/service/ReactiveService.java
+++ src/main/java/com/learning/webflux/reactiveapi/service/ReactiveService.java
@@ -1,7 +1,9 @@
 package com.learning.webflux.reactiveapi.service;
 
+import com.learning.webflux.reactiveapi.engine.Engine;
 import com.learning.webflux.reactiveapi.model.CatGene;
 import com.learning.webflux.reactiveapi.model.ReactiveResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -11,6 +13,10 @@ import java.util.*;
 
 @Component
 public class ReactiveService {
+
+    @Autowired
+    Engine engine;
+    public static final String FIRST_GEN = "firstGen";
 
 //    public Mono<ReactiveResponse> computeMessage() {
 //        return Mono.just("Hello World")
@@ -24,7 +30,7 @@ public class ReactiveService {
     // APR
     // V or F
 
-    public Mono<ReactiveResponse> retrieveGenesResponse(){
+    public Mono<ReactiveResponse> retrieveGenesResponse() {
         return retrieveGenes()
                 .flatMap(list -> filterList(list)).log()
                 .map(listed -> new ReactiveResponse(listed)).log();
@@ -33,38 +39,128 @@ public class ReactiveService {
 //                .flatMap(filteredList -> new );
     }
 
+//    public Mono<ReactiveResponse> retrieveGenesResponse2() {
+//        return Mono.from(new ReactiveResponse(
+//                        retrieveGenes().flatMap(list -> engine.geneProcessor(list))
+//        )
+//    }
+
     private Mono<List<CatGene>> filterList(List<CatGene> catGenes) {
-        return Mono.just(catGenes.stream().filter(gene -> "firstGen".equals(gene.getGeneration())).toList());
+        return Mono.just(catGenes.stream().filter(gene -> FIRST_GEN.equals(gene.getGeneration())).toList());
     }
 
     private Mono<List<CatGene>> filterList2(List<CatGene> catGenes) {
-        return Mono.just(catGenes.stream().filter(gene -> "firstGen".equals(gene.getGeneration())).toList());
+//        return catGenes.map(list -> list.stream().filter(catGene -> FIRST_GEN.equals(catGene.getGeneration())).toList());
+        return Mono.from(retrieveGenes());
     }
 
-//    private Map<String, List<CatGene>> mapOfMaps(List<CatGene> catGenes){
-//        Map<String, List<CatGene>> firstGen = new HashMap<>();
-//        Map<String, List<CatGene>> secondGen = new HashMap<>();
-//
-//        for(CatGene gene : catGenes){
-//            if("secondGen".equals(gene.getGeneration())){
-//                secondGen.put(gene.getId(), gene);
-//            }
-//        }
-//
-//        return null;
-//
-//    }
+
+    private Map<String, List<CatGene>> mapOfMaps(List<CatGene> catGenes) {
+        Map<String, List<CatGene>> masterMap = new HashMap<>();
+
+        for (CatGene gene : catGenes) {
+            masterMap.computeIfAbsent(gene.getGeneration(),
+                    g -> new ArrayList<>()).add(gene);
+        }
+
+        return masterMap;
+
+    }
 
     private Mono<List<CatGene>> retrieveGenes() {
         return Mono.just(Arrays.asList(
                 new CatGene(1l, // accountKey
-                        "firstGen", //viewType
+                        FIRST_GEN, //viewType
                         "longhair", // RT - long hair
                         "Main Coon", //RSN
                         1000l, //APR
                         Date.from(Instant.now())),
                 new CatGene(1l, // accountKey
                         "secondGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "thirdGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "fourthGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        FIRST_GEN, //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1000l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "secondGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "thirdGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "fourthGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        FIRST_GEN, //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1000l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "secondGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "thirdGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "fourthGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        FIRST_GEN, //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1000l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "secondGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "thirdGen", //viewType
+                        "longhair", // RT - long hair
+                        "Main Coon", //RSN
+                        1250l, //APR
+                        Date.from(Instant.now())),
+                new CatGene(1l, // accountKey
+                        "fourthGen", //viewType
                         "longhair", // RT - long hair
                         "Main Coon", //RSN
                         1250l, //APR
